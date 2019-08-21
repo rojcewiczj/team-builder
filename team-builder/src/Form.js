b813b9822e5a93037feaa9b89099b6f02cd6ce0a
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-21 15:13:29 -0500

    working on form

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 
 const teamMemberForm =props => {
     const [form, setForm] = useState({name: '', email: '', role: ''})
-    return;
+    const changeHandler = event => {
+        console.log(event.target.value);
+        setForm({ ...form, [event.target.name]: event.target.value });
+    };
+    const submitMember = event => {
+        event.preventDefault();
+        
+    }
 }
 
 
