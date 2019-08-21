709c1ff315a178ef4dca432d86629256df481381
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-21 15:08:12 -0500

    builtout memberCard

--- team-builder/src/MemberCard.js
+++ team-builder/src/MemberCard.js
@@ -4,7 +4,15 @@ const Cards = props => {
     console.log(props);
     return (
         <div className="member-card">
-            {props.memberCard.map ()}
+            {props.memberCard.map(member => {
+                return(
+                    <div className="card" key={member.id}>
+                        <h1>{member.name}</h1>
+                        <h3>{member.role}</h3>
+                        <h5>{member.email}</h5>
+                        </div>
+                );
+            })}
 
         </div>
     )
